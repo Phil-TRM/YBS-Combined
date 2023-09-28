aab7f6649256ecaedf9f3ec365b2979c61360fef
Phil-TRM <134960021+Phil-TRM@users.noreply.github.com>
2023-09-27 20:22:23 -0400

    Community User creation and other front end fixes

--- backend/util/createCommunityUser.js
+++ backend/util/createCommunityUser.js
@@ -2,19 +2,17 @@ const axios = require('axios')
 const COMMUNITY_SITE_BASE_URL = "https://communities.yourbestself-ie.com/api/v1";
 
 const JSON_HEADER = {
-    "Content-Type": "application/json",
-    'Accept':'application/json',
-   
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Authorization': 'Bearer YFLDucNdLftF-9xoIH4gTl_60pfQ_5bMTcM2EHJubotEQubitUHN3kgEQYeg9vIvHbMvxZKRapKbIVTgqgak21' //Prod
 }
 
 const createCommunityUser = async (data) => {
-    console.log("data", data);
-    return () => {}
-    // uncomment when create is fully flushed out
-    // return axios.post(COMMUNITY_SITE_BASE_URL+"/user", {
-    //     headers: JSON_HEADER,
-    //     body: JSON.stringify(data),
-    // });
+    axios.post(`${COMMUNITY_SITE_BASE_URL}/user`, data, {
+      headers: JSON_HEADER,
+    }).then(
+      res => console.log("Response",res)
+    ).catch(err => console.log("Error", err.response.data));
 }
 
-module.exports = createCommunityUser
+  module.exports = createCommunityUser
